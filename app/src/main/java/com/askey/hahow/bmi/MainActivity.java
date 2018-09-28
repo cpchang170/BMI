1139dbc476e5f936937c05008de4d06128a9bcdf
cp_chang <cp_chang@bitbucket.com.tw>
2018-09-28 18:01:28 +0800

    Add Dialog builder

--- app/src/main/java/com/askey/hahow/bmi/MainActivity.java
+++ app/src/main/java/com/askey/hahow/bmi/MainActivity.java
@@ -1,28 +1,61 @@
 package com.askey.hahow.bmi;
 
+import android.content.DialogInterface;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edw;
+    private EditText edh;
+    private TextView result;
+    private Button help;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FindViews();
     }
+
+    private void FindViews() {
+        edw = findViewById(R.id.ed_weight);
+        edh = findViewById(R.id.ed_height);
+        result = findViewById(R.id.result);
+        help = findViewById(R.id.help);
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this).setTitle("HELP").setMessage("The body mass index (BMI) or Quetelet index is a value derived from the mass (weight) and height of an individual").setPositiveButton("OK", null).show();
+            }
+        });
+    }
+
     public void bmi(View view){
-        EditText edw = findViewById(R.id.ed_weight);
-        EditText edh = findViewById(R.id.ed_height);
-        String s_edw = edw.toString();
+        String s_edw = edw.getText().toString();
         //Log.d("MainActivity","BMI" + s_edw);
-        String s_edh = edh.toString();
+        String s_edh = edh.getText().toString();
         //Log.d("MainActivity","BMI" + s_edh);
         float w = Float.parseFloat(s_edw);
         float h = Float.parseFloat(s_edh);
-        //float bmi = w / (h * h);
-        Log.d("MainActivity", "BMI");
+        float bmi = w / (h * h);
+        Log.d("MainActivity", "BMI" + bmi);
+        Toast.makeText(this,"your BMI is " + bmi,Toast.LENGTH_LONG).show();
+        result.setText("you BMI is " + bmi);
+        new AlertDialog.Builder(this).setTitle("BMI").setMessage("your BMI is"+bmi).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                edw.setText("");
+                edh.setText("");
+            }
+        }).show();
     }
 }
